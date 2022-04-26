319e7b00f8f9716eb777a7a62bd0f11bd365ad8d
yanjiyi <630758569@qq.com>
2022-04-26 17:29:12 +0800

    first

---
+++ js/utils.js
@@ -0,0 +1,22 @@
+function timestampToTime(timestamp) {
+    var date = new Date(timestamp);//时间戳为10位需*1000，时间戳为13位的话不需乘1000
+    Y = date.getFullYear() + '-';
+    M = (date.getMonth()+1 < 10 ? '0'+(date.getMonth()+1) : date.getMonth()+1) + '-';
+    D = date.getDate() + ' ';
+    h = date.getHours() + ':';
+    m = date.getMinutes() + ':';
+    s = date.getSeconds();
+    return Y+M+D+" "+h+m+s;
+}
+
+function getFileSize(fileByte) {
+    var fileSizeByte = fileByte;
+    var fileSizeMsg = "";
+    if (fileSizeByte < 1048576) fileSizeMsg = (fileSizeByte / 1024).toFixed(2) + "KB";
+    else if (fileSizeByte == 1048576) fileSizeMsg = "1MB";
+    else if (fileSizeByte > 1048576 && fileSizeByte < 1073741824) fileSizeMsg = (fileSizeByte / (1024 * 1024)).toFixed(2) + "MB";
+    else if (fileSizeByte > 1048576 && fileSizeByte == 1073741824) fileSizeMsg = "1GB";
+    else if (fileSizeByte > 1073741824 && fileSizeByte < 1099511627776) fileSizeMsg = (fileSizeByte / (1024 * 1024 * 1024)).toFixed(2) + "GB";
+    else fileSizeMsg = "文件超过1TB";
+    return fileSizeMsg;
+}
